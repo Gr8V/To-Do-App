8655e4c4ce6f39b8a5948db48a1d78c0ad7d6bf8
Gr8V <vanshdangi0045@gmail.com>
2024-03-02 01:25:18 +0530

    Finalized input command

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-class Program{
-    public string command = "";
+public class Program{
     public static void Main(string[] args)
     {
         Console.WriteLine("Enter what you want to do (eg. Add Task/ Remove Task/ Change Task Status/ View Tasks) :--");
-        command = Console.ReadLine();
-        
+        string Rawcommand = Console.ReadLine();
+        string NoSpaceCommand = Rawcommand.Replace(" ", "");
+        string command = NoSpaceCommand.ToLower();
         Console.ReadKey();
     }
 }
