5aabef6b15a3231e31f7d44fea2975f9072047bf
Gr8V <91793876+Gr8V@users.noreply.github.com>
2024-03-02 01:14:16 +0530

    Take User Input

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 
 class Program{
+    public string command = "";
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello");
+        Console.WriteLine("Enter what you want to do (eg. Add Task/ Remove Task/ Change Task Status/ View Tasks) :--");
+        command = Console.ReadLine();
         
         Console.ReadKey();
     }
